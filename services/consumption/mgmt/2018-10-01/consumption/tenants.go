a80d7eb67e244faf2a54b370ea9bc563b4710230
Phoenix He <tih@microsoft.com>
2019-08-07 18:30:31 +0800

    Modifications after running after_scripts

--- services/consumption/mgmt/2018-10-01/consumption/tenants.go
+++ services/consumption/mgmt/2018-10-01/consumption/tenants.go
@@ -18,101 +18,101 @@ package consumption
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-    "github.com/Azure/go-autorest/autorest"
-    "github.com/Azure/go-autorest/autorest/azure"
-    "net/http"
-    "context"
-    "github.com/Azure/go-autorest/tracing"
+	"context"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/go-autorest/tracing"
+	"net/http"
 )
 
 // TenantsClient is the consumption management client provides access to consumption resources for Azure Enterprise
 // Subscriptions.
 type TenantsClient struct {
-    BaseClient
+	BaseClient
 }
+
 // NewTenantsClient creates an instance of the TenantsClient client.
 func NewTenantsClient(subscriptionID string) TenantsClient {
-    return NewTenantsClientWithBaseURI(DefaultBaseURI, subscriptionID)
+	return NewTenantsClientWithBaseURI(DefaultBaseURI, subscriptionID)
 }
 
 // NewTenantsClientWithBaseURI creates an instance of the TenantsClient client.
-    func NewTenantsClientWithBaseURI(baseURI string, subscriptionID string) TenantsClient {
-        return TenantsClient{ NewWithBaseURI(baseURI, subscriptionID)}
-    }
+func NewTenantsClientWithBaseURI(baseURI string, subscriptionID string) TenantsClient {
+	return TenantsClient{NewWithBaseURI(baseURI, subscriptionID)}
+}
 
 // Get gets a Tenant Properties.
-    // Parameters:
-        // billingAccountID - billing Account Id.
-        // billingProfileID - billing Profile Id.
+// Parameters:
+// billingAccountID - billing Account Id.
+// billingProfileID - billing Profile Id.
 func (client TenantsClient) Get(ctx context.Context, billingAccountID string, billingProfileID string) (result TenantListResult, err error) {
-    if tracing.IsEnabled() {
-        ctx = tracing.StartSpan(ctx, fqdn + "/TenantsClient.Get")
-        defer func() {
-            sc := -1
-            if result.Response.Response != nil {
-                sc = result.Response.Response.StatusCode
-            }
-            tracing.EndSpan(ctx, sc, err)
-        }()
-    }
-        req, err := client.GetPreparer(ctx, billingAccountID, billingProfileID)
-    if err != nil {
-    err = autorest.NewErrorWithError(err, "consumption.TenantsClient", "Get", nil , "Failure preparing request")
-    return
-    }
+	if tracing.IsEnabled() {
+		ctx = tracing.StartSpan(ctx, fqdn+"/TenantsClient.Get")
+		defer func() {
+			sc := -1
+			if result.Response.Response != nil {
+				sc = result.Response.Response.StatusCode
+			}
+			tracing.EndSpan(ctx, sc, err)
+		}()
+	}
+	req, err := client.GetPreparer(ctx, billingAccountID, billingProfileID)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "consumption.TenantsClient", "Get", nil, "Failure preparing request")
+		return
+	}
 
-            resp, err := client.GetSender(req)
-            if err != nil {
-            result.Response = autorest.Response{Response: resp}
-            err = autorest.NewErrorWithError(err, "consumption.TenantsClient", "Get", resp, "Failure sending request")
-            return
-            }
+	resp, err := client.GetSender(req)
+	if err != nil {
+		result.Response = autorest.Response{Response: resp}
+		err = autorest.NewErrorWithError(err, "consumption.TenantsClient", "Get", resp, "Failure sending request")
+		return
+	}
 
-            result, err = client.GetResponder(resp)
-            if err != nil {
-            err = autorest.NewErrorWithError(err, "consumption.TenantsClient", "Get", resp, "Failure responding to request")
-            }
+	result, err = client.GetResponder(resp)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "consumption.TenantsClient", "Get", resp, "Failure responding to request")
+	}
 
-    return
-    }
+	return
+}
 
-    // GetPreparer prepares the Get request.
-    func (client TenantsClient) GetPreparer(ctx context.Context, billingAccountID string, billingProfileID string) (*http.Request, error) {
-            pathParameters := map[string]interface{} {
-            "billingAccountId": autorest.Encode("path",billingAccountID),
-            "billingProfileId": autorest.Encode("path",billingProfileID),
-            }
+// GetPreparer prepares the Get request.
+func (client TenantsClient) GetPreparer(ctx context.Context, billingAccountID string, billingProfileID string) (*http.Request, error) {
+	pathParameters := map[string]interface{}{
+		"billingAccountId": autorest.Encode("path", billingAccountID),
+		"billingProfileId": autorest.Encode("path", billingProfileID),
+	}
 
-                        const APIVersion = "2018-10-01"
-        queryParameters := map[string]interface{} {
-        "api-version": APIVersion,
-        }
+	const APIVersion = "2018-10-01"
+	queryParameters := map[string]interface{}{
+		"api-version": APIVersion,
+	}
 
-        preparer := autorest.CreatePreparer(
-    autorest.AsGet(),
-    autorest.WithBaseURL(client.BaseURI),
-    autorest.WithPathParameters("/providers/Microsoft.Billing/billingAccounts/{billingAccountId}/billingProfiles/{billingProfileId}/providers/Microsoft.Consumption/tenants",pathParameters),
-    autorest.WithQueryParameters(queryParameters))
-    return preparer.Prepare((&http.Request{}).WithContext(ctx))
-    }
+	preparer := autorest.CreatePreparer(
+		autorest.AsGet(),
+		autorest.WithBaseURL(client.BaseURI),
+		autorest.WithPathParameters("/providers/Microsoft.Billing/billingAccounts/{billingAccountId}/billingProfiles/{billingProfileId}/providers/Microsoft.Consumption/tenants", pathParameters),
+		autorest.WithQueryParameters(queryParameters))
+	return preparer.Prepare((&http.Request{}).WithContext(ctx))
+}
 
-    // GetSender sends the Get request. The method will close the
-    // http.Response Body if it receives an error.
-    func (client TenantsClient) GetSender(req *http.Request) (*http.Response, error) {
-        sd := autorest.GetSendDecorators(req.Context(), autorest.DoRetryForStatusCodes(client.RetryAttempts, client.RetryDuration, autorest.StatusCodesForRetry...))
-            return autorest.SendWithSender(client, req, sd...)
-            }
+// GetSender sends the Get request. The method will close the
+// http.Response Body if it receives an error.
+func (client TenantsClient) GetSender(req *http.Request) (*http.Response, error) {
+	sd := autorest.GetSendDecorators(req.Context(), autorest.DoRetryForStatusCodes(client.RetryAttempts, client.RetryDuration, autorest.StatusCodesForRetry...))
+	return autorest.SendWithSender(client, req, sd...)
+}
 
 // GetResponder handles the response to the Get request. The method always
 // closes the http.Response Body.
 func (client TenantsClient) GetResponder(resp *http.Response) (result TenantListResult, err error) {
-    err = autorest.Respond(
-    resp,
-    client.ByInspecting(),
-    azure.WithErrorUnlessStatusCode(http.StatusOK),
-    autorest.ByUnmarshallingJSON(&result),
-    autorest.ByClosing())
-    result.Response = autorest.Response{Response: resp}
-        return
-    }
-
+	err = autorest.Respond(
+		resp,
+		client.ByInspecting(),
+		azure.WithErrorUnlessStatusCode(http.StatusOK),
+		autorest.ByUnmarshallingJSON(&result),
+		autorest.ByClosing())
+	result.Response = autorest.Response{Response: resp}
+	return
+}
