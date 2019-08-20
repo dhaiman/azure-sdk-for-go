9388a00cc44cb76c419fa2cfd23cb171cb9d0ed6
SDK Automation <sdkautomation@microsoft.com>
2019-08-20 02:09:12 +0000

    Modifications after running after_scripts

--- profiles/preview/preview/subscription/mgmt/subscription/models.go
+++ profiles/preview/preview/subscription/mgmt/subscription/models.go
@@ -68,8 +68,12 @@ type Location = original.Location
 type LocationListResult = original.LocationListResult
 type Model = original.Model
 type Operation = original.Operation
+type OperationDisplay = original.OperationDisplay
 type OperationListResult = original.OperationListResult
+type OperationListResultType = original.OperationListResultType
+type OperationType = original.OperationType
 type OperationsClient = original.OperationsClient
+type OperationsGroupClient = original.OperationsGroupClient
 type Policies = original.Policies
 type SubscriptionsClient = original.SubscriptionsClient
 type TenantIDDescription = original.TenantIDDescription
@@ -98,6 +102,12 @@ func NewOperationsClient() OperationsClient {
 }
 func NewOperationsClientWithBaseURI(baseURI string) OperationsClient {
 	return original.NewOperationsClientWithBaseURI(baseURI)
+}
+func NewOperationsGroupClient() OperationsGroupClient {
+	return original.NewOperationsGroupClient()
+}
+func NewOperationsGroupClientWithBaseURI(baseURI string) OperationsGroupClient {
+	return original.NewOperationsGroupClientWithBaseURI(baseURI)
 }
 func NewSubscriptionsClient() SubscriptionsClient {
 	return original.NewSubscriptionsClient()
