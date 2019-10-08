d5ede69071aa79d9f58a55b4eabcd84e0123774d
SDK Automation <sdkautomation@microsoft.com>
2019-10-08 14:36:58 +0000

    Modifications after running after_scripts

--- services/preview/peering/mgmt/2019-09-01-preview/peering/serviceproviders.go
+++ services/preview/peering/mgmt/2019-09-01-preview/peering/serviceproviders.go
@@ -18,134 +18,134 @@ package peering
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-    "github.com/Azure/go-autorest/autorest"
-    "github.com/Azure/go-autorest/autorest/azure"
-    "net/http"
-    "context"
-    "github.com/Azure/go-autorest/tracing"
+	"context"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/go-autorest/tracing"
+	"net/http"
 )
 
 // ServiceProvidersClient is the peering Client
 type ServiceProvidersClient struct {
-    BaseClient
+	BaseClient
 }
+
 // NewServiceProvidersClient creates an instance of the ServiceProvidersClient client.
 func NewServiceProvidersClient(subscriptionID string) ServiceProvidersClient {
-    return NewServiceProvidersClientWithBaseURI(DefaultBaseURI, subscriptionID)
+	return NewServiceProvidersClientWithBaseURI(DefaultBaseURI, subscriptionID)
 }
 
 // NewServiceProvidersClientWithBaseURI creates an instance of the ServiceProvidersClient client.
-    func NewServiceProvidersClientWithBaseURI(baseURI string, subscriptionID string) ServiceProvidersClient {
-        return ServiceProvidersClient{ NewWithBaseURI(baseURI, subscriptionID)}
-    }
+func NewServiceProvidersClientWithBaseURI(baseURI string, subscriptionID string) ServiceProvidersClient {
+	return ServiceProvidersClient{NewWithBaseURI(baseURI, subscriptionID)}
+}
 
 // List lists all of the available peering service locations for the specified kind of peering.
 func (client ServiceProvidersClient) List(ctx context.Context) (result ServiceProviderListResultPage, err error) {
-    if tracing.IsEnabled() {
-        ctx = tracing.StartSpan(ctx, fqdn + "/ServiceProvidersClient.List")
-        defer func() {
-            sc := -1
-            if result.splr.Response.Response != nil {
-                sc = result.splr.Response.Response.StatusCode
-            }
-            tracing.EndSpan(ctx, sc, err)
-        }()
-    }
-                result.fn = client.listNextResults
-    req, err := client.ListPreparer(ctx)
-    if err != nil {
-    err = autorest.NewErrorWithError(err, "peering.ServiceProvidersClient", "List", nil , "Failure preparing request")
-    return
-    }
+	if tracing.IsEnabled() {
+		ctx = tracing.StartSpan(ctx, fqdn+"/ServiceProvidersClient.List")
+		defer func() {
+			sc := -1
+			if result.splr.Response.Response != nil {
+				sc = result.splr.Response.Response.StatusCode
+			}
+			tracing.EndSpan(ctx, sc, err)
+		}()
+	}
+	result.fn = client.listNextResults
+	req, err := client.ListPreparer(ctx)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "peering.ServiceProvidersClient", "List", nil, "Failure preparing request")
+		return
+	}
 
-            resp, err := client.ListSender(req)
-            if err != nil {
-            result.splr.Response = autorest.Response{Response: resp}
-            err = autorest.NewErrorWithError(err, "peering.ServiceProvidersClient", "List", resp, "Failure sending request")
-            return
-            }
+	resp, err := client.ListSender(req)
+	if err != nil {
+		result.splr.Response = autorest.Response{Response: resp}
+		err = autorest.NewErrorWithError(err, "peering.ServiceProvidersClient", "List", resp, "Failure sending request")
+		return
+	}
 
-            result.splr, err = client.ListResponder(resp)
-            if err != nil {
-            err = autorest.NewErrorWithError(err, "peering.ServiceProvidersClient", "List", resp, "Failure responding to request")
-            }
+	result.splr, err = client.ListResponder(resp)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "peering.ServiceProvidersClient", "List", resp, "Failure responding to request")
+	}
 
-    return
-    }
+	return
+}
 
-    // ListPreparer prepares the List request.
-    func (client ServiceProvidersClient) ListPreparer(ctx context.Context) (*http.Request, error) {
-            pathParameters := map[string]interface{} {
-            "subscriptionId": autorest.Encode("path",client.SubscriptionID),
-            }
+// ListPreparer prepares the List request.
+func (client ServiceProvidersClient) ListPreparer(ctx context.Context) (*http.Request, error) {
+	pathParameters := map[string]interface{}{
+		"subscriptionId": autorest.Encode("path", client.SubscriptionID),
+	}
 
-                        const APIVersion = "2019-09-01-preview"
-        queryParameters := map[string]interface{} {
-        "api-version": APIVersion,
-        }
+	const APIVersion = "2019-09-01-preview"
+	queryParameters := map[string]interface{}{
+		"api-version": APIVersion,
+	}
 
-        preparer := autorest.CreatePreparer(
-    autorest.AsGet(),
-    autorest.WithBaseURL(client.BaseURI),
-    autorest.WithPathParameters("/subscriptions/{subscriptionId}/providers/Microsoft.Peering/peeringServiceProviders",pathParameters),
-    autorest.WithQueryParameters(queryParameters))
-    return preparer.Prepare((&http.Request{}).WithContext(ctx))
-    }
+	preparer := autorest.CreatePreparer(
+		autorest.AsGet(),
+		autorest.WithBaseURL(client.BaseURI),
+		autorest.WithPathParameters("/subscriptions/{subscriptionId}/providers/Microsoft.Peering/peeringServiceProviders", pathParameters),
+		autorest.WithQueryParameters(queryParameters))
+	return preparer.Prepare((&http.Request{}).WithContext(ctx))
+}
 
-    // ListSender sends the List request. The method will close the
-    // http.Response Body if it receives an error.
-    func (client ServiceProvidersClient) ListSender(req *http.Request) (*http.Response, error) {
-        sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
-            return autorest.SendWithSender(client, req, sd...)
-            }
+// ListSender sends the List request. The method will close the
+// http.Response Body if it receives an error.
+func (client ServiceProvidersClient) ListSender(req *http.Request) (*http.Response, error) {
+	sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
+	return autorest.SendWithSender(client, req, sd...)
+}
 
 // ListResponder handles the response to the List request. The method always
 // closes the http.Response Body.
 func (client ServiceProvidersClient) ListResponder(resp *http.Response) (result ServiceProviderListResult, err error) {
-    err = autorest.Respond(
-    resp,
-    client.ByInspecting(),
-    azure.WithErrorUnlessStatusCode(http.StatusOK),
-    autorest.ByUnmarshallingJSON(&result),
-    autorest.ByClosing())
-    result.Response = autorest.Response{Response: resp}
-        return
-    }
+	err = autorest.Respond(
+		resp,
+		client.ByInspecting(),
+		azure.WithErrorUnlessStatusCode(http.StatusOK),
+		autorest.ByUnmarshallingJSON(&result),
+		autorest.ByClosing())
+	result.Response = autorest.Response{Response: resp}
+	return
+}
 
-            // listNextResults retrieves the next set of results, if any.
-            func (client ServiceProvidersClient) listNextResults(ctx context.Context, lastResults ServiceProviderListResult) (result ServiceProviderListResult, err error) {
-            req, err := lastResults.serviceProviderListResultPreparer(ctx)
-            if err != nil {
-            return result, autorest.NewErrorWithError(err, "peering.ServiceProvidersClient", "listNextResults", nil , "Failure preparing next results request")
-            }
-            if req == nil {
-            return
-            }
-            resp, err := client.ListSender(req)
-            if err != nil {
-            result.Response = autorest.Response{Response: resp}
-            return result, autorest.NewErrorWithError(err, "peering.ServiceProvidersClient", "listNextResults", resp, "Failure sending next results request")
-            }
-            result, err = client.ListResponder(resp)
-            if err != nil {
-            err = autorest.NewErrorWithError(err, "peering.ServiceProvidersClient", "listNextResults", resp, "Failure responding to next results request")
-            }
-            return
-                    }
+// listNextResults retrieves the next set of results, if any.
+func (client ServiceProvidersClient) listNextResults(ctx context.Context, lastResults ServiceProviderListResult) (result ServiceProviderListResult, err error) {
+	req, err := lastResults.serviceProviderListResultPreparer(ctx)
+	if err != nil {
+		return result, autorest.NewErrorWithError(err, "peering.ServiceProvidersClient", "listNextResults", nil, "Failure preparing next results request")
+	}
+	if req == nil {
+		return
+	}
+	resp, err := client.ListSender(req)
+	if err != nil {
+		result.Response = autorest.Response{Response: resp}
+		return result, autorest.NewErrorWithError(err, "peering.ServiceProvidersClient", "listNextResults", resp, "Failure sending next results request")
+	}
+	result, err = client.ListResponder(resp)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "peering.ServiceProvidersClient", "listNextResults", resp, "Failure responding to next results request")
+	}
+	return
+}
 
-    // ListComplete enumerates all values, automatically crossing page boundaries as required.
-    func (client ServiceProvidersClient) ListComplete(ctx context.Context) (result ServiceProviderListResultIterator, err error) {
-        if tracing.IsEnabled() {
-            ctx = tracing.StartSpan(ctx, fqdn + "/ServiceProvidersClient.List")
-            defer func() {
-                sc := -1
-                if result.Response().Response.Response != nil {
-                    sc = result.page.Response().Response.Response.StatusCode
-                }
-                tracing.EndSpan(ctx, sc, err)
-            }()
-     }
-        result.page, err = client.List(ctx)
-                return
-        }
-
+// ListComplete enumerates all values, automatically crossing page boundaries as required.
+func (client ServiceProvidersClient) ListComplete(ctx context.Context) (result ServiceProviderListResultIterator, err error) {
+	if tracing.IsEnabled() {
+		ctx = tracing.StartSpan(ctx, fqdn+"/ServiceProvidersClient.List")
+		defer func() {
+			sc := -1
+			if result.Response().Response.Response != nil {
+				sc = result.page.Response().Response.Response.StatusCode
+			}
+			tracing.EndSpan(ctx, sc, err)
+		}()
+	}
+	result.page, err = client.List(ctx)
+	return
+}
