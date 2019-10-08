7f1f3ca5dab681c54bc0c18b0554281f8d853659
SDK Automation <sdkautomation@microsoft.com>
2019-10-08 04:58:16 +0000

    Generated from 48d7c9bd3570f631dc7be25113842487f35dc89f
    chore: jsonfmt cost-management

--- services/preview/costmanagement/mgmt/2019-03-01/costmanagement/showbackrules.go
+++ services/preview/costmanagement/mgmt/2019-03-01/costmanagement/showbackrules.go
@@ -18,98 +18,98 @@ package costmanagement
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-	"context"
-	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/azure"
-	"github.com/Azure/go-autorest/tracing"
-	"net/http"
+    "github.com/Azure/go-autorest/autorest"
+    "github.com/Azure/go-autorest/autorest/azure"
+    "net/http"
+    "context"
+    "github.com/Azure/go-autorest/tracing"
 )
 
 // ShowbackRulesClient is the client for the ShowbackRules methods of the Costmanagement service.
 type ShowbackRulesClient struct {
-	BaseClient
+    BaseClient
 }
-
 // NewShowbackRulesClient creates an instance of the ShowbackRulesClient client.
 func NewShowbackRulesClient(subscriptionID string) ShowbackRulesClient {
-	return NewShowbackRulesClientWithBaseURI(DefaultBaseURI, subscriptionID)
+    return NewShowbackRulesClientWithBaseURI(DefaultBaseURI, subscriptionID)
 }
 
 // NewShowbackRulesClientWithBaseURI creates an instance of the ShowbackRulesClient client.
-func NewShowbackRulesClientWithBaseURI(baseURI string, subscriptionID string) ShowbackRulesClient {
-	return ShowbackRulesClient{NewWithBaseURI(baseURI, subscriptionID)}
-}
+    func NewShowbackRulesClientWithBaseURI(baseURI string, subscriptionID string) ShowbackRulesClient {
+        return ShowbackRulesClient{ NewWithBaseURI(baseURI, subscriptionID)}
+    }
 
 // List get list all Showback Rules.
-// Parameters:
-// billingAccountID - billingAccount ID
+    // Parameters:
+        // billingAccountID - billingAccount ID
 func (client ShowbackRulesClient) List(ctx context.Context, billingAccountID string) (result ShowbackRuleListResult, err error) {
-	if tracing.IsEnabled() {
-		ctx = tracing.StartSpan(ctx, fqdn+"/ShowbackRulesClient.List")
-		defer func() {
-			sc := -1
-			if result.Response.Response != nil {
-				sc = result.Response.Response.StatusCode
-			}
-			tracing.EndSpan(ctx, sc, err)
-		}()
-	}
-	req, err := client.ListPreparer(ctx, billingAccountID)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "costmanagement.ShowbackRulesClient", "List", nil, "Failure preparing request")
-		return
-	}
+    if tracing.IsEnabled() {
+        ctx = tracing.StartSpan(ctx, fqdn + "/ShowbackRulesClient.List")
+        defer func() {
+            sc := -1
+            if result.Response.Response != nil {
+                sc = result.Response.Response.StatusCode
+            }
+            tracing.EndSpan(ctx, sc, err)
+        }()
+    }
+        req, err := client.ListPreparer(ctx, billingAccountID)
+    if err != nil {
+    err = autorest.NewErrorWithError(err, "costmanagement.ShowbackRulesClient", "List", nil , "Failure preparing request")
+    return
+    }
 
-	resp, err := client.ListSender(req)
-	if err != nil {
-		result.Response = autorest.Response{Response: resp}
-		err = autorest.NewErrorWithError(err, "costmanagement.ShowbackRulesClient", "List", resp, "Failure sending request")
-		return
-	}
+            resp, err := client.ListSender(req)
+            if err != nil {
+            result.Response = autorest.Response{Response: resp}
+            err = autorest.NewErrorWithError(err, "costmanagement.ShowbackRulesClient", "List", resp, "Failure sending request")
+            return
+            }
 
-	result, err = client.ListResponder(resp)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "costmanagement.ShowbackRulesClient", "List", resp, "Failure responding to request")
-	}
+            result, err = client.ListResponder(resp)
+            if err != nil {
+            err = autorest.NewErrorWithError(err, "costmanagement.ShowbackRulesClient", "List", resp, "Failure responding to request")
+            }
 
-	return
-}
+    return
+    }
 
-// ListPreparer prepares the List request.
-func (client ShowbackRulesClient) ListPreparer(ctx context.Context, billingAccountID string) (*http.Request, error) {
-	pathParameters := map[string]interface{}{
-		"billingAccountId": autorest.Encode("path", billingAccountID),
-	}
+    // ListPreparer prepares the List request.
+    func (client ShowbackRulesClient) ListPreparer(ctx context.Context, billingAccountID string) (*http.Request, error) {
+            pathParameters := map[string]interface{} {
+            "billingAccountId": autorest.Encode("path",billingAccountID),
+            }
 
-	const APIVersion = "2019-03-01-preview"
-	queryParameters := map[string]interface{}{
-		"api-version": APIVersion,
-	}
+                        const APIVersion = "2019-03-01-preview"
+        queryParameters := map[string]interface{} {
+        "api-version": APIVersion,
+        }
 
-	preparer := autorest.CreatePreparer(
-		autorest.AsGet(),
-		autorest.WithBaseURL(client.BaseURI),
-		autorest.WithPathParameters("/providers/Microsoft.Billing/billingAccounts/{billingAccountId}/providers/Microsoft.CostManagement/showbackRules", pathParameters),
-		autorest.WithQueryParameters(queryParameters))
-	return preparer.Prepare((&http.Request{}).WithContext(ctx))
-}
+        preparer := autorest.CreatePreparer(
+    autorest.AsGet(),
+    autorest.WithBaseURL(client.BaseURI),
+    autorest.WithPathParameters("/providers/Microsoft.Billing/billingAccounts/{billingAccountId}/providers/Microsoft.CostManagement/showbackRules",pathParameters),
+    autorest.WithQueryParameters(queryParameters))
+    return preparer.Prepare((&http.Request{}).WithContext(ctx))
+    }
 
-// ListSender sends the List request. The method will close the
-// http.Response Body if it receives an error.
-func (client ShowbackRulesClient) ListSender(req *http.Request) (*http.Response, error) {
-	sd := autorest.GetSendDecorators(req.Context(), autorest.DoRetryForStatusCodes(client.RetryAttempts, client.RetryDuration, autorest.StatusCodesForRetry...))
-	return autorest.SendWithSender(client, req, sd...)
-}
+    // ListSender sends the List request. The method will close the
+    // http.Response Body if it receives an error.
+    func (client ShowbackRulesClient) ListSender(req *http.Request) (*http.Response, error) {
+        sd := autorest.GetSendDecorators(req.Context(), autorest.DoRetryForStatusCodes(client.RetryAttempts, client.RetryDuration, autorest.StatusCodesForRetry...))
+            return autorest.SendWithSender(client, req, sd...)
+            }
 
 // ListResponder handles the response to the List request. The method always
 // closes the http.Response Body.
 func (client ShowbackRulesClient) ListResponder(resp *http.Response) (result ShowbackRuleListResult, err error) {
-	err = autorest.Respond(
-		resp,
-		client.ByInspecting(),
-		azure.WithErrorUnlessStatusCode(http.StatusOK),
-		autorest.ByUnmarshallingJSON(&result),
-		autorest.ByClosing())
-	result.Response = autorest.Response{Response: resp}
-	return
-}
+    err = autorest.Respond(
+    resp,
+    client.ByInspecting(),
+    azure.WithErrorUnlessStatusCode(http.StatusOK),
+    autorest.ByUnmarshallingJSON(&result),
+    autorest.ByClosing())
+    result.Response = autorest.Response{Response: resp}
+        return
+    }
+
