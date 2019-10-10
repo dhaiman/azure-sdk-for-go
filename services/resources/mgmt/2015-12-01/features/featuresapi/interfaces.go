6cc02f587cd8a85d712d7cd728b5235d3084c4b2
SDK Automation <sdkautomation@microsoft.com>
2019-10-10 07:36:21 +0000

    Modifications after running after_scripts

--- services/resources/mgmt/2015-12-01/features/featuresapi/interfaces.go
+++ services/resources/mgmt/2015-12-01/features/featuresapi/interfaces.go
@@ -18,22 +18,23 @@ package featuresapi
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-    "context"
-    "github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2015-12-01/features"
+	"context"
+	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2015-12-01/features"
 )
 
-        // BaseClientAPI contains the set of methods on the BaseClient type.
-        type BaseClientAPI interface {
-            ListOperations(ctx context.Context) (result features.OperationListResultPage, err error)
-        }
+// BaseClientAPI contains the set of methods on the BaseClient type.
+type BaseClientAPI interface {
+	ListOperations(ctx context.Context) (result features.OperationListResultPage, err error)
+}
 
-        var _ BaseClientAPI = (*features.BaseClient)(nil)
-        // ClientAPI contains the set of methods on the Client type.
-        type ClientAPI interface {
-            Get(ctx context.Context, resourceProviderNamespace string, featureName string) (result features.Result, err error)
-            List(ctx context.Context, resourceProviderNamespace string) (result features.OperationsListResultPage, err error)
-            ListAll(ctx context.Context) (result features.OperationsListResultPage, err error)
-            Register(ctx context.Context, resourceProviderNamespace string, featureName string) (result features.Result, err error)
-        }
+var _ BaseClientAPI = (*features.BaseClient)(nil)
 
-        var _ ClientAPI = (*features.Client)(nil)
+// ClientAPI contains the set of methods on the Client type.
+type ClientAPI interface {
+	Get(ctx context.Context, resourceProviderNamespace string, featureName string) (result features.Result, err error)
+	List(ctx context.Context, resourceProviderNamespace string) (result features.OperationsListResultPage, err error)
+	ListAll(ctx context.Context) (result features.OperationsListResultPage, err error)
+	Register(ctx context.Context, resourceProviderNamespace string, featureName string) (result features.Result, err error)
+}
+
+var _ ClientAPI = (*features.Client)(nil)
