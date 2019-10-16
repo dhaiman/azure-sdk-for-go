693afac5431fa6beafd1d45e05d4385569dbc8f0
SDK Automation <sdkautomation@microsoft.com>
2019-10-16 19:06:24 +0000

    Generated from 9ff123a05a77ee05cfec173ebbcb57417685ab6a
    Updating the verbs from Create BillingProfile and InvoiceSection entities
    
    Updating the verbs from Create BillingProfile and InvoiceSection entities

--- services/preview/billing/mgmt/2018-11-01-preview/billing/availablebalances.go
+++ services/preview/billing/mgmt/2018-11-01-preview/billing/availablebalances.go
@@ -18,100 +18,100 @@ package billing
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-	"context"
-	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/azure"
-	"github.com/Azure/go-autorest/tracing"
-	"net/http"
+    "github.com/Azure/go-autorest/autorest"
+    "github.com/Azure/go-autorest/autorest/azure"
+    "net/http"
+    "context"
+    "github.com/Azure/go-autorest/tracing"
 )
 
 // AvailableBalancesClient is the billing client provides access to billing resources for Azure subscriptions.
 type AvailableBalancesClient struct {
-	BaseClient
+    BaseClient
 }
-
 // NewAvailableBalancesClient creates an instance of the AvailableBalancesClient client.
 func NewAvailableBalancesClient(subscriptionID string) AvailableBalancesClient {
-	return NewAvailableBalancesClientWithBaseURI(DefaultBaseURI, subscriptionID)
+    return NewAvailableBalancesClientWithBaseURI(DefaultBaseURI, subscriptionID)
 }
 
 // NewAvailableBalancesClientWithBaseURI creates an instance of the AvailableBalancesClient client.
-func NewAvailableBalancesClientWithBaseURI(baseURI string, subscriptionID string) AvailableBalancesClient {
-	return AvailableBalancesClient{NewWithBaseURI(baseURI, subscriptionID)}
-}
+    func NewAvailableBalancesClientWithBaseURI(baseURI string, subscriptionID string) AvailableBalancesClient {
+        return AvailableBalancesClient{ NewWithBaseURI(baseURI, subscriptionID)}
+    }
 
 // GetByBillingProfile the latest available credit balance for a given billingAccountName and billingProfileName.
-// Parameters:
-// billingAccountName - billing Account Id.
-// billingProfileName - billing Profile Id.
+    // Parameters:
+        // billingAccountName - billing Account Id.
+        // billingProfileName - billing Profile Id.
 func (client AvailableBalancesClient) GetByBillingProfile(ctx context.Context, billingAccountName string, billingProfileName string) (result AvailableBalance, err error) {
-	if tracing.IsEnabled() {
-		ctx = tracing.StartSpan(ctx, fqdn+"/AvailableBalancesClient.GetByBillingProfile")
-		defer func() {
-			sc := -1
-			if result.Response.Response != nil {
-				sc = result.Response.Response.StatusCode
-			}
-			tracing.EndSpan(ctx, sc, err)
-		}()
-	}
-	req, err := client.GetByBillingProfilePreparer(ctx, billingAccountName, billingProfileName)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "billing.AvailableBalancesClient", "GetByBillingProfile", nil, "Failure preparing request")
-		return
-	}
+    if tracing.IsEnabled() {
+        ctx = tracing.StartSpan(ctx, fqdn + "/AvailableBalancesClient.GetByBillingProfile")
+        defer func() {
+            sc := -1
+            if result.Response.Response != nil {
+                sc = result.Response.Response.StatusCode
+            }
+            tracing.EndSpan(ctx, sc, err)
+        }()
+    }
+        req, err := client.GetByBillingProfilePreparer(ctx, billingAccountName, billingProfileName)
+    if err != nil {
+    err = autorest.NewErrorWithError(err, "billing.AvailableBalancesClient", "GetByBillingProfile", nil , "Failure preparing request")
+    return
+    }
 
-	resp, err := client.GetByBillingProfileSender(req)
-	if err != nil {
-		result.Response = autorest.Response{Response: resp}
-		err = autorest.NewErrorWithError(err, "billing.AvailableBalancesClient", "GetByBillingProfile", resp, "Failure sending request")
-		return
-	}
+            resp, err := client.GetByBillingProfileSender(req)
+            if err != nil {
+            result.Response = autorest.Response{Response: resp}
+            err = autorest.NewErrorWithError(err, "billing.AvailableBalancesClient", "GetByBillingProfile", resp, "Failure sending request")
+            return
+            }
 
-	result, err = client.GetByBillingProfileResponder(resp)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "billing.AvailableBalancesClient", "GetByBillingProfile", resp, "Failure responding to request")
-	}
+            result, err = client.GetByBillingProfileResponder(resp)
+            if err != nil {
+            err = autorest.NewErrorWithError(err, "billing.AvailableBalancesClient", "GetByBillingProfile", resp, "Failure responding to request")
+            }
 
-	return
-}
+    return
+    }
 
-// GetByBillingProfilePreparer prepares the GetByBillingProfile request.
-func (client AvailableBalancesClient) GetByBillingProfilePreparer(ctx context.Context, billingAccountName string, billingProfileName string) (*http.Request, error) {
-	pathParameters := map[string]interface{}{
-		"billingAccountName": autorest.Encode("path", billingAccountName),
-		"billingProfileName": autorest.Encode("path", billingProfileName),
-	}
+    // GetByBillingProfilePreparer prepares the GetByBillingProfile request.
+    func (client AvailableBalancesClient) GetByBillingProfilePreparer(ctx context.Context, billingAccountName string, billingProfileName string) (*http.Request, error) {
+            pathParameters := map[string]interface{} {
+            "billingAccountName": autorest.Encode("path",billingAccountName),
+            "billingProfileName": autorest.Encode("path",billingProfileName),
+            }
 
-	const APIVersion = "2018-11-01-preview"
-	queryParameters := map[string]interface{}{
-		"api-version": APIVersion,
-	}
+                        const APIVersion = "2018-11-01-preview"
+        queryParameters := map[string]interface{} {
+        "api-version": APIVersion,
+        }
 
-	preparer := autorest.CreatePreparer(
-		autorest.AsGet(),
-		autorest.WithBaseURL(client.BaseURI),
-		autorest.WithPathParameters("/providers/Microsoft.Billing/billingAccounts/{billingAccountName}/billingProfiles/{billingProfileName}/availableBalance/default", pathParameters),
-		autorest.WithQueryParameters(queryParameters))
-	return preparer.Prepare((&http.Request{}).WithContext(ctx))
-}
+        preparer := autorest.CreatePreparer(
+    autorest.AsGet(),
+    autorest.WithBaseURL(client.BaseURI),
+    autorest.WithPathParameters("/providers/Microsoft.Billing/billingAccounts/{billingAccountName}/billingProfiles/{billingProfileName}/availableBalance/default",pathParameters),
+    autorest.WithQueryParameters(queryParameters))
+    return preparer.Prepare((&http.Request{}).WithContext(ctx))
+    }
 
-// GetByBillingProfileSender sends the GetByBillingProfile request. The method will close the
-// http.Response Body if it receives an error.
-func (client AvailableBalancesClient) GetByBillingProfileSender(req *http.Request) (*http.Response, error) {
-	sd := autorest.GetSendDecorators(req.Context(), autorest.DoRetryForStatusCodes(client.RetryAttempts, client.RetryDuration, autorest.StatusCodesForRetry...))
-	return autorest.SendWithSender(client, req, sd...)
-}
+    // GetByBillingProfileSender sends the GetByBillingProfile request. The method will close the
+    // http.Response Body if it receives an error.
+    func (client AvailableBalancesClient) GetByBillingProfileSender(req *http.Request) (*http.Response, error) {
+        sd := autorest.GetSendDecorators(req.Context(), autorest.DoRetryForStatusCodes(client.RetryAttempts, client.RetryDuration, autorest.StatusCodesForRetry...))
+            return autorest.SendWithSender(client, req, sd...)
+            }
 
 // GetByBillingProfileResponder handles the response to the GetByBillingProfile request. The method always
 // closes the http.Response Body.
 func (client AvailableBalancesClient) GetByBillingProfileResponder(resp *http.Response) (result AvailableBalance, err error) {
-	err = autorest.Respond(
-		resp,
-		client.ByInspecting(),
-		azure.WithErrorUnlessStatusCode(http.StatusOK),
-		autorest.ByUnmarshallingJSON(&result),
-		autorest.ByClosing())
-	result.Response = autorest.Response{Response: resp}
-	return
-}
+    err = autorest.Respond(
+    resp,
+    client.ByInspecting(),
+    azure.WithErrorUnlessStatusCode(http.StatusOK),
+    autorest.ByUnmarshallingJSON(&result),
+    autorest.ByClosing())
+    result.Response = autorest.Response{Response: resp}
+        return
+    }
+
