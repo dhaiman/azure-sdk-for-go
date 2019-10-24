5cd2b8d21511349a8332fe068a0f4655b91fe8ec
SDK Automation <sdkautomation@microsoft.com>
2019-10-24 00:58:05 +0000

    Generated from 3fc94818ac2da5a6baa4f56bc6b9cf5f9c2835e5
    Change ResourceIdentity to EncryptionSetIdentity

--- services/preview/containerservice/mgmt/2015-11-01-preview/containerservice/containerserviceapi/interfaces.go
+++ services/preview/containerservice/mgmt/2015-11-01-preview/containerservice/containerserviceapi/interfaces.go
@@ -18,16 +18,16 @@ package containerserviceapi
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-	"context"
-	"github.com/Azure/azure-sdk-for-go/services/preview/containerservice/mgmt/2015-11-01-preview/containerservice"
+    "context"
+    "github.com/Azure/azure-sdk-for-go/services/preview/containerservice/mgmt/2015-11-01-preview/containerservice"
 )
 
-// ClientAPI contains the set of methods on the Client type.
-type ClientAPI interface {
-	CreateOrUpdate(ctx context.Context, resourceGroupName string, containerServiceName string, parameters containerservice.ContainerService) (result containerservice.CreateOrUpdateFuture, err error)
-	Delete(ctx context.Context, resourceGroupName string, containerServiceName string) (result containerservice.DeleteFuture, err error)
-	Get(ctx context.Context, resourceGroupName string, containerServiceName string) (result containerservice.ContainerService, err error)
-	ListByResourceGroup(ctx context.Context, resourceGroupName string) (result containerservice.ListResult, err error)
-}
+        // ClientAPI contains the set of methods on the Client type.
+        type ClientAPI interface {
+            CreateOrUpdate(ctx context.Context, resourceGroupName string, containerServiceName string, parameters containerservice.ContainerService) (result containerservice.CreateOrUpdateFuture, err error)
+            Delete(ctx context.Context, resourceGroupName string, containerServiceName string) (result containerservice.DeleteFuture, err error)
+            Get(ctx context.Context, resourceGroupName string, containerServiceName string) (result containerservice.ContainerService, err error)
+            ListByResourceGroup(ctx context.Context, resourceGroupName string) (result containerservice.ListResult, err error)
+        }
 
-var _ ClientAPI = (*containerservice.Client)(nil)
+        var _ ClientAPI = (*containerservice.Client)(nil)
