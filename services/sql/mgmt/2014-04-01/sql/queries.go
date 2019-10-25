59e437735bb0969bd6868370efdeb71b54d51a93
SDK Automation <sdkautomation@microsoft.com>
2019-10-25 17:59:13 +0000

    Generated from 4e1c6d6196b4d0e56d884dfa21b12f57db9c2698
    update readme.md with correct types

--- services/sql/mgmt/2014-04-01/sql/queries.go
+++ services/sql/mgmt/2014-04-01/sql/queries.go
@@ -18,106 +18,106 @@ package sql
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-	"context"
-	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/azure"
-	"github.com/Azure/go-autorest/tracing"
-	"net/http"
+    "github.com/Azure/go-autorest/autorest"
+    "github.com/Azure/go-autorest/autorest/azure"
+    "net/http"
+    "context"
+    "github.com/Azure/go-autorest/tracing"
 )
 
 // QueriesClient is the the Azure SQL Database management API provides a RESTful set of web services that interact with
 // Azure SQL Database services to manage your databases. The API enables you to create, retrieve, update, and delete
 // databases.
 type QueriesClient struct {
-	BaseClient
+    BaseClient
 }
-
 // NewQueriesClient creates an instance of the QueriesClient client.
 func NewQueriesClient(subscriptionID string) QueriesClient {
-	return NewQueriesClientWithBaseURI(DefaultBaseURI, subscriptionID)
+    return NewQueriesClientWithBaseURI(DefaultBaseURI, subscriptionID)
 }
 
 // NewQueriesClientWithBaseURI creates an instance of the QueriesClient client.
-func NewQueriesClientWithBaseURI(baseURI string, subscriptionID string) QueriesClient {
-	return QueriesClient{NewWithBaseURI(baseURI, subscriptionID)}
-}
+    func NewQueriesClientWithBaseURI(baseURI string, subscriptionID string) QueriesClient {
+        return QueriesClient{ NewWithBaseURI(baseURI, subscriptionID)}
+    }
 
 // ListByDatabase gets a list of top queries by database.
-// Parameters:
-// resourceGroupName - the name of the resource group that contains the resource. You can obtain this value
-// from the Azure Resource Manager API or the portal.
-// serverName - the name of the server.
-// databaseName - the name of the database.
+    // Parameters:
+        // resourceGroupName - the name of the resource group that contains the resource. You can obtain this value
+        // from the Azure Resource Manager API or the portal.
+        // serverName - the name of the server.
+        // databaseName - the name of the database.
 func (client QueriesClient) ListByDatabase(ctx context.Context, resourceGroupName string, serverName string, databaseName string) (result TopQueriesListResult, err error) {
-	if tracing.IsEnabled() {
-		ctx = tracing.StartSpan(ctx, fqdn+"/QueriesClient.ListByDatabase")
-		defer func() {
-			sc := -1
-			if result.Response.Response != nil {
-				sc = result.Response.Response.StatusCode
-			}
-			tracing.EndSpan(ctx, sc, err)
-		}()
-	}
-	req, err := client.ListByDatabasePreparer(ctx, resourceGroupName, serverName, databaseName)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "sql.QueriesClient", "ListByDatabase", nil, "Failure preparing request")
-		return
-	}
+    if tracing.IsEnabled() {
+        ctx = tracing.StartSpan(ctx, fqdn + "/QueriesClient.ListByDatabase")
+        defer func() {
+            sc := -1
+            if result.Response.Response != nil {
+                sc = result.Response.Response.StatusCode
+            }
+            tracing.EndSpan(ctx, sc, err)
+        }()
+    }
+        req, err := client.ListByDatabasePreparer(ctx, resourceGroupName, serverName, databaseName)
+    if err != nil {
+    err = autorest.NewErrorWithError(err, "sql.QueriesClient", "ListByDatabase", nil , "Failure preparing request")
+    return
+    }
 
-	resp, err := client.ListByDatabaseSender(req)
-	if err != nil {
-		result.Response = autorest.Response{Response: resp}
-		err = autorest.NewErrorWithError(err, "sql.QueriesClient", "ListByDatabase", resp, "Failure sending request")
-		return
-	}
+            resp, err := client.ListByDatabaseSender(req)
+            if err != nil {
+            result.Response = autorest.Response{Response: resp}
+            err = autorest.NewErrorWithError(err, "sql.QueriesClient", "ListByDatabase", resp, "Failure sending request")
+            return
+            }
 
-	result, err = client.ListByDatabaseResponder(resp)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "sql.QueriesClient", "ListByDatabase", resp, "Failure responding to request")
-	}
+            result, err = client.ListByDatabaseResponder(resp)
+            if err != nil {
+            err = autorest.NewErrorWithError(err, "sql.QueriesClient", "ListByDatabase", resp, "Failure responding to request")
+            }
 
-	return
-}
+    return
+    }
 
-// ListByDatabasePreparer prepares the ListByDatabase request.
-func (client QueriesClient) ListByDatabasePreparer(ctx context.Context, resourceGroupName string, serverName string, databaseName string) (*http.Request, error) {
-	pathParameters := map[string]interface{}{
-		"databaseName":      autorest.Encode("path", databaseName),
-		"resourceGroupName": autorest.Encode("path", resourceGroupName),
-		"serverName":        autorest.Encode("path", serverName),
-		"subscriptionId":    autorest.Encode("path", client.SubscriptionID),
-	}
+    // ListByDatabasePreparer prepares the ListByDatabase request.
+    func (client QueriesClient) ListByDatabasePreparer(ctx context.Context, resourceGroupName string, serverName string, databaseName string) (*http.Request, error) {
+            pathParameters := map[string]interface{} {
+            "databaseName": autorest.Encode("path",databaseName),
+            "resourceGroupName": autorest.Encode("path",resourceGroupName),
+            "serverName": autorest.Encode("path",serverName),
+            "subscriptionId": autorest.Encode("path",client.SubscriptionID),
+            }
 
-	const APIVersion = "2014-04-01"
-	queryParameters := map[string]interface{}{
-		"api-version": APIVersion,
-	}
+                        const APIVersion = "2014-04-01"
+        queryParameters := map[string]interface{} {
+        "api-version": APIVersion,
+        }
 
-	preparer := autorest.CreatePreparer(
-		autorest.AsGet(),
-		autorest.WithBaseURL(client.BaseURI),
-		autorest.WithPathParameters("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/databases/{databaseName}/topQueries", pathParameters),
-		autorest.WithQueryParameters(queryParameters))
-	return preparer.Prepare((&http.Request{}).WithContext(ctx))
-}
+        preparer := autorest.CreatePreparer(
+    autorest.AsGet(),
+    autorest.WithBaseURL(client.BaseURI),
+    autorest.WithPathParameters("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/databases/{databaseName}/topQueries",pathParameters),
+    autorest.WithQueryParameters(queryParameters))
+    return preparer.Prepare((&http.Request{}).WithContext(ctx))
+    }
 
-// ListByDatabaseSender sends the ListByDatabase request. The method will close the
-// http.Response Body if it receives an error.
-func (client QueriesClient) ListByDatabaseSender(req *http.Request) (*http.Response, error) {
-	sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
-	return autorest.SendWithSender(client, req, sd...)
-}
+    // ListByDatabaseSender sends the ListByDatabase request. The method will close the
+    // http.Response Body if it receives an error.
+    func (client QueriesClient) ListByDatabaseSender(req *http.Request) (*http.Response, error) {
+        sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
+            return autorest.SendWithSender(client, req, sd...)
+            }
 
 // ListByDatabaseResponder handles the response to the ListByDatabase request. The method always
 // closes the http.Response Body.
 func (client QueriesClient) ListByDatabaseResponder(resp *http.Response) (result TopQueriesListResult, err error) {
-	err = autorest.Respond(
-		resp,
-		client.ByInspecting(),
-		azure.WithErrorUnlessStatusCode(http.StatusOK),
-		autorest.ByUnmarshallingJSON(&result),
-		autorest.ByClosing())
-	result.Response = autorest.Response{Response: resp}
-	return
-}
+    err = autorest.Respond(
+    resp,
+    client.ByInspecting(),
+    azure.WithErrorUnlessStatusCode(http.StatusOK),
+    autorest.ByUnmarshallingJSON(&result),
+    autorest.ByClosing())
+    result.Response = autorest.Response{Response: resp}
+        return
+    }
+
