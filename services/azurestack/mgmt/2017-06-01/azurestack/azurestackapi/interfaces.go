baa45d466f8a342c4545c6d9a1bec85a641ce937
SDK Automation <sdkautomation@microsoft.com>
2019-10-25 20:06:28 +0000

    Modifications after running after_scripts

--- services/azurestack/mgmt/2017-06-01/azurestack/azurestackapi/interfaces.go
+++ services/azurestack/mgmt/2017-06-01/azurestack/azurestackapi/interfaces.go
@@ -18,55 +18,60 @@ package azurestackapi
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-    "context"
-    "github.com/Azure/azure-sdk-for-go/services/azurestack/mgmt/2017-06-01/azurestack"
-    "github.com/Azure/go-autorest/autorest"
+	"context"
+	"github.com/Azure/azure-sdk-for-go/services/azurestack/mgmt/2017-06-01/azurestack"
+	"github.com/Azure/go-autorest/autorest"
 )
 
-        // BaseClientAPI contains the set of methods on the BaseClient type.
-        type BaseClientAPI interface {
-            GetProduct(ctx context.Context, resourceGroup string, registrationName string, productName string, deviceConfiguration *azurestack.DeviceConfiguration) (result azurestack.Product, err error)
-            Uploadproductlog(ctx context.Context, resourceGroup string, registrationName string, productName string, marketplaceProductLogUpdate *azurestack.MarketplaceProductLogUpdate) (result azurestack.ProductLog, err error)
-        }
+// BaseClientAPI contains the set of methods on the BaseClient type.
+type BaseClientAPI interface {
+	GetProduct(ctx context.Context, resourceGroup string, registrationName string, productName string, deviceConfiguration *azurestack.DeviceConfiguration) (result azurestack.Product, err error)
+	Uploadproductlog(ctx context.Context, resourceGroup string, registrationName string, productName string, marketplaceProductLogUpdate *azurestack.MarketplaceProductLogUpdate) (result azurestack.ProductLog, err error)
+}
 
-        var _ BaseClientAPI = (*azurestack.BaseClient)(nil)
-        // OperationsClientAPI contains the set of methods on the OperationsClient type.
-        type OperationsClientAPI interface {
-            List(ctx context.Context) (result azurestack.OperationListPage, err error)
-        }
+var _ BaseClientAPI = (*azurestack.BaseClient)(nil)
 
-        var _ OperationsClientAPI = (*azurestack.OperationsClient)(nil)
-        // ProductsClientAPI contains the set of methods on the ProductsClient type.
-        type ProductsClientAPI interface {
-            Get(ctx context.Context, resourceGroup string, registrationName string, productName string) (result azurestack.Product, err error)
-            List(ctx context.Context, resourceGroup string, registrationName string) (result azurestack.ProductListPage, err error)
-            ListDetails(ctx context.Context, resourceGroup string, registrationName string, productName string) (result azurestack.ExtendedProduct, err error)
-        }
+// OperationsClientAPI contains the set of methods on the OperationsClient type.
+type OperationsClientAPI interface {
+	List(ctx context.Context) (result azurestack.OperationListPage, err error)
+}
 
-        var _ ProductsClientAPI = (*azurestack.ProductsClient)(nil)
-        // GetProductsClientAPI contains the set of methods on the GetProductsClient type.
-        type GetProductsClientAPI interface {
-            List(ctx context.Context, resourceGroup string, registrationName string, deviceConfiguration *azurestack.DeviceConfiguration) (result azurestack.ProductList, err error)
-        }
+var _ OperationsClientAPI = (*azurestack.OperationsClient)(nil)
 
-        var _ GetProductsClientAPI = (*azurestack.GetProductsClient)(nil)
-        // RegistrationsClientAPI contains the set of methods on the RegistrationsClient type.
-        type RegistrationsClientAPI interface {
-            CreateOrUpdate(ctx context.Context, resourceGroup string, registrationName string, tokenParameter azurestack.RegistrationParameter) (result azurestack.Registration, err error)
-            Delete(ctx context.Context, resourceGroup string, registrationName string) (result autorest.Response, err error)
-            Get(ctx context.Context, resourceGroup string, registrationName string) (result azurestack.Registration, err error)
-            GetActivationKey(ctx context.Context, resourceGroup string, registrationName string) (result azurestack.ActivationKeyResult, err error)
-            List(ctx context.Context, resourceGroup string) (result azurestack.RegistrationListPage, err error)
-            Update(ctx context.Context, resourceGroup string, registrationName string, tokenParameter azurestack.RegistrationParameter) (result azurestack.Registration, err error)
-        }
+// ProductsClientAPI contains the set of methods on the ProductsClient type.
+type ProductsClientAPI interface {
+	Get(ctx context.Context, resourceGroup string, registrationName string, productName string) (result azurestack.Product, err error)
+	List(ctx context.Context, resourceGroup string, registrationName string) (result azurestack.ProductListPage, err error)
+	ListDetails(ctx context.Context, resourceGroup string, registrationName string, productName string) (result azurestack.ExtendedProduct, err error)
+}
 
-        var _ RegistrationsClientAPI = (*azurestack.RegistrationsClient)(nil)
-        // CustomerSubscriptionsClientAPI contains the set of methods on the CustomerSubscriptionsClient type.
-        type CustomerSubscriptionsClientAPI interface {
-            Create(ctx context.Context, resourceGroup string, registrationName string, customerSubscriptionName string, customerCreationParameters azurestack.CustomerSubscription) (result azurestack.CustomerSubscription, err error)
-            Delete(ctx context.Context, resourceGroup string, registrationName string, customerSubscriptionName string) (result autorest.Response, err error)
-            Get(ctx context.Context, resourceGroup string, registrationName string, customerSubscriptionName string) (result azurestack.CustomerSubscription, err error)
-            List(ctx context.Context, resourceGroup string, registrationName string) (result azurestack.CustomerSubscriptionListPage, err error)
-        }
+var _ ProductsClientAPI = (*azurestack.ProductsClient)(nil)
 
-        var _ CustomerSubscriptionsClientAPI = (*azurestack.CustomerSubscriptionsClient)(nil)
+// GetProductsClientAPI contains the set of methods on the GetProductsClient type.
+type GetProductsClientAPI interface {
+	List(ctx context.Context, resourceGroup string, registrationName string, deviceConfiguration *azurestack.DeviceConfiguration) (result azurestack.ProductList, err error)
+}
+
+var _ GetProductsClientAPI = (*azurestack.GetProductsClient)(nil)
+
+// RegistrationsClientAPI contains the set of methods on the RegistrationsClient type.
+type RegistrationsClientAPI interface {
+	CreateOrUpdate(ctx context.Context, resourceGroup string, registrationName string, tokenParameter azurestack.RegistrationParameter) (result azurestack.Registration, err error)
+	Delete(ctx context.Context, resourceGroup string, registrationName string) (result autorest.Response, err error)
+	Get(ctx context.Context, resourceGroup string, registrationName string) (result azurestack.Registration, err error)
+	GetActivationKey(ctx context.Context, resourceGroup string, registrationName string) (result azurestack.ActivationKeyResult, err error)
+	List(ctx context.Context, resourceGroup string) (result azurestack.RegistrationListPage, err error)
+	Update(ctx context.Context, resourceGroup string, registrationName string, tokenParameter azurestack.RegistrationParameter) (result azurestack.Registration, err error)
+}
+
+var _ RegistrationsClientAPI = (*azurestack.RegistrationsClient)(nil)
+
+// CustomerSubscriptionsClientAPI contains the set of methods on the CustomerSubscriptionsClient type.
+type CustomerSubscriptionsClientAPI interface {
+	Create(ctx context.Context, resourceGroup string, registrationName string, customerSubscriptionName string, customerCreationParameters azurestack.CustomerSubscription) (result azurestack.CustomerSubscription, err error)
+	Delete(ctx context.Context, resourceGroup string, registrationName string, customerSubscriptionName string) (result autorest.Response, err error)
+	Get(ctx context.Context, resourceGroup string, registrationName string, customerSubscriptionName string) (result azurestack.CustomerSubscription, err error)
+	List(ctx context.Context, resourceGroup string, registrationName string) (result azurestack.CustomerSubscriptionListPage, err error)
+}
+
+var _ CustomerSubscriptionsClientAPI = (*azurestack.CustomerSubscriptionsClient)(nil)
--- services/azurestack/mgmt/2017-06-01/azurestack/getproducts.go
+++ services/azurestack/mgmt/2017-06-01/azurestack/getproducts.go
@@ -18,109 +18,109 @@ package azurestack
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-    "github.com/Azure/go-autorest/autorest"
-    "github.com/Azure/go-autorest/autorest/azure"
-    "net/http"
-    "context"
-    "github.com/Azure/go-autorest/tracing"
+	"context"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/go-autorest/tracing"
+	"net/http"
 )
 
 // GetProductsClient is the azure Stack
 type GetProductsClient struct {
-    BaseClient
+	BaseClient
 }
+
 // NewGetProductsClient creates an instance of the GetProductsClient client.
 func NewGetProductsClient(subscriptionID string) GetProductsClient {
-    return NewGetProductsClientWithBaseURI(DefaultBaseURI, subscriptionID)
+	return NewGetProductsClientWithBaseURI(DefaultBaseURI, subscriptionID)
 }
 
 // NewGetProductsClientWithBaseURI creates an instance of the GetProductsClient client.
-    func NewGetProductsClientWithBaseURI(baseURI string, subscriptionID string) GetProductsClient {
-        return GetProductsClient{ NewWithBaseURI(baseURI, subscriptionID)}
-    }
+func NewGetProductsClientWithBaseURI(baseURI string, subscriptionID string) GetProductsClient {
+	return GetProductsClient{NewWithBaseURI(baseURI, subscriptionID)}
+}
 
 // List returns a list of products.
-    // Parameters:
-        // resourceGroup - name of the resource group.
-        // registrationName - name of the Azure Stack registration.
-        // deviceConfiguration - device configuration.
+// Parameters:
+// resourceGroup - name of the resource group.
+// registrationName - name of the Azure Stack registration.
+// deviceConfiguration - device configuration.
 func (client GetProductsClient) List(ctx context.Context, resourceGroup string, registrationName string, deviceConfiguration *DeviceConfiguration) (result ProductList, err error) {
-    if tracing.IsEnabled() {
-        ctx = tracing.StartSpan(ctx, fqdn + "/GetProductsClient.List")
-        defer func() {
-            sc := -1
-            if result.Response.Response != nil {
-                sc = result.Response.Response.StatusCode
-            }
-            tracing.EndSpan(ctx, sc, err)
-        }()
-    }
-        req, err := client.ListPreparer(ctx, resourceGroup, registrationName, deviceConfiguration)
-    if err != nil {
-    err = autorest.NewErrorWithError(err, "azurestack.GetProductsClient", "List", nil , "Failure preparing request")
-    return
-    }
+	if tracing.IsEnabled() {
+		ctx = tracing.StartSpan(ctx, fqdn+"/GetProductsClient.List")
+		defer func() {
+			sc := -1
+			if result.Response.Response != nil {
+				sc = result.Response.Response.StatusCode
+			}
+			tracing.EndSpan(ctx, sc, err)
+		}()
+	}
+	req, err := client.ListPreparer(ctx, resourceGroup, registrationName, deviceConfiguration)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "azurestack.GetProductsClient", "List", nil, "Failure preparing request")
+		return
+	}
 
-            resp, err := client.ListSender(req)
-            if err != nil {
-            result.Response = autorest.Response{Response: resp}
-            err = autorest.NewErrorWithError(err, "azurestack.GetProductsClient", "List", resp, "Failure sending request")
-            return
-            }
+	resp, err := client.ListSender(req)
+	if err != nil {
+		result.Response = autorest.Response{Response: resp}
+		err = autorest.NewErrorWithError(err, "azurestack.GetProductsClient", "List", resp, "Failure sending request")
+		return
+	}
 
-            result, err = client.ListResponder(resp)
-            if err != nil {
-            err = autorest.NewErrorWithError(err, "azurestack.GetProductsClient", "List", resp, "Failure responding to request")
-            }
+	result, err = client.ListResponder(resp)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "azurestack.GetProductsClient", "List", resp, "Failure responding to request")
+	}
 
-    return
-    }
+	return
+}
 
-    // ListPreparer prepares the List request.
-    func (client GetProductsClient) ListPreparer(ctx context.Context, resourceGroup string, registrationName string, deviceConfiguration *DeviceConfiguration) (*http.Request, error) {
-            pathParameters := map[string]interface{} {
-            "registrationName": autorest.Encode("path",registrationName),
-            "resourceGroup": autorest.Encode("path",resourceGroup),
-            "subscriptionId": autorest.Encode("path",client.SubscriptionID),
-            }
+// ListPreparer prepares the List request.
+func (client GetProductsClient) ListPreparer(ctx context.Context, resourceGroup string, registrationName string, deviceConfiguration *DeviceConfiguration) (*http.Request, error) {
+	pathParameters := map[string]interface{}{
+		"registrationName": autorest.Encode("path", registrationName),
+		"resourceGroup":    autorest.Encode("path", resourceGroup),
+		"subscriptionId":   autorest.Encode("path", client.SubscriptionID),
+	}
 
-                        const APIVersion = "2017-06-01"
-        queryParameters := map[string]interface{} {
-        "api-version": APIVersion,
-        }
+	const APIVersion = "2017-06-01"
+	queryParameters := map[string]interface{}{
+		"api-version": APIVersion,
+	}
 
-                    deviceConfiguration.DeviceVersion = nil
-                deviceConfiguration.IdentitySystem = ""
-    preparer := autorest.CreatePreparer(
-    autorest.AsContentType("application/json; charset=utf-8"),
-    autorest.AsPost(),
-    autorest.WithBaseURL(client.BaseURI),
-    autorest.WithPathParameters("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroup}/providers/Microsoft.AzureStack/registrations/{registrationName}/products/_all/GetProducts",pathParameters),
-    autorest.WithQueryParameters(queryParameters))
-            if deviceConfiguration != nil {
-            preparer = autorest.DecoratePreparer(preparer,
-            autorest.WithJSON(deviceConfiguration))
-            }
-    return preparer.Prepare((&http.Request{}).WithContext(ctx))
-    }
+	deviceConfiguration.DeviceVersion = nil
+	deviceConfiguration.IdentitySystem = ""
+	preparer := autorest.CreatePreparer(
+		autorest.AsContentType("application/json; charset=utf-8"),
+		autorest.AsPost(),
+		autorest.WithBaseURL(client.BaseURI),
+		autorest.WithPathParameters("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroup}/providers/Microsoft.AzureStack/registrations/{registrationName}/products/_all/GetProducts", pathParameters),
+		autorest.WithQueryParameters(queryParameters))
+	if deviceConfiguration != nil {
+		preparer = autorest.DecoratePreparer(preparer,
+			autorest.WithJSON(deviceConfiguration))
+	}
+	return preparer.Prepare((&http.Request{}).WithContext(ctx))
+}
 
-    // ListSender sends the List request. The method will close the
-    // http.Response Body if it receives an error.
-    func (client GetProductsClient) ListSender(req *http.Request) (*http.Response, error) {
-        sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
-            return autorest.SendWithSender(client, req, sd...)
-            }
+// ListSender sends the List request. The method will close the
+// http.Response Body if it receives an error.
+func (client GetProductsClient) ListSender(req *http.Request) (*http.Response, error) {
+	sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
+	return autorest.SendWithSender(client, req, sd...)
+}
 
 // ListResponder handles the response to the List request. The method always
 // closes the http.Response Body.
 func (client GetProductsClient) ListResponder(resp *http.Response) (result ProductList, err error) {
-    err = autorest.Respond(
-    resp,
-    client.ByInspecting(),
-    azure.WithErrorUnlessStatusCode(http.StatusOK),
-    autorest.ByUnmarshallingJSON(&result),
-    autorest.ByClosing())
-    result.Response = autorest.Response{Response: resp}
-        return
-    }
-
+	err = autorest.Respond(
+		resp,
+		client.ByInspecting(),
+		azure.WithErrorUnlessStatusCode(http.StatusOK),
+		autorest.ByUnmarshallingJSON(&result),
+		autorest.ByClosing())
+	result.Response = autorest.Response{Response: resp}
+	return
+}
