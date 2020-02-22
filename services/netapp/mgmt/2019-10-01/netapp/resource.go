245450167248decd3051bf563ddd2b232af6728d
SDK Automation <sdkautomation@microsoft.com>
2020-02-22 00:33:54 +0000

    Generated from e7d6a97e97f16becc1635f198a2389401dee75d9
    Merge branch 'firewallpolicy_with_ipgroups' of https://github.com/ssripadham/azure-rest-api-specs into firewallpolicy_with_ipgroups

--- services/netapp/mgmt/2019-10-01/netapp/resource.go
+++ services/netapp/mgmt/2019-10-01/netapp/resource.go
@@ -18,196 +18,194 @@ package netapp
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-	"context"
-	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/azure"
-	"github.com/Azure/go-autorest/autorest/validation"
-	"github.com/Azure/go-autorest/tracing"
-	"net/http"
+    "github.com/Azure/go-autorest/autorest"
+    "github.com/Azure/go-autorest/autorest/azure"
+    "net/http"
+    "context"
+    "github.com/Azure/go-autorest/tracing"
+    "github.com/Azure/go-autorest/autorest/validation"
 )
 
 // ResourceClient is the microsoft NetApp Azure Resource Provider specification
 type ResourceClient struct {
-	BaseClient
+    BaseClient
 }
-
 // NewResourceClient creates an instance of the ResourceClient client.
 func NewResourceClient(subscriptionID string) ResourceClient {
-	return NewResourceClientWithBaseURI(DefaultBaseURI, subscriptionID)
+    return NewResourceClientWithBaseURI(DefaultBaseURI, subscriptionID)
 }
 
 // NewResourceClientWithBaseURI creates an instance of the ResourceClient client using a custom endpoint.  Use this
 // when interacting with an Azure cloud that uses a non-standard base URI (sovereign clouds, Azure stack).
-func NewResourceClientWithBaseURI(baseURI string, subscriptionID string) ResourceClient {
-	return ResourceClient{NewWithBaseURI(baseURI, subscriptionID)}
-}
+    func NewResourceClientWithBaseURI(baseURI string, subscriptionID string) ResourceClient {
+        return ResourceClient{ NewWithBaseURI(baseURI, subscriptionID)}
+    }
 
 // CheckFilePathAvailability check if a file path is available.
-// Parameters:
-// body - file path availability request.
-// location - the location
+    // Parameters:
+        // body - file path availability request.
+        // location - the location
 func (client ResourceClient) CheckFilePathAvailability(ctx context.Context, body ResourceNameAvailabilityRequest, location string) (result ResourceNameAvailability, err error) {
-	if tracing.IsEnabled() {
-		ctx = tracing.StartSpan(ctx, fqdn+"/ResourceClient.CheckFilePathAvailability")
-		defer func() {
-			sc := -1
-			if result.Response.Response != nil {
-				sc = result.Response.Response.StatusCode
-			}
-			tracing.EndSpan(ctx, sc, err)
-		}()
-	}
-	if err := validation.Validate([]validation.Validation{
-		{TargetValue: body,
-			Constraints: []validation.Constraint{{Target: "body.Name", Name: validation.Null, Rule: true, Chain: nil},
-				{Target: "body.ResourceGroup", Name: validation.Null, Rule: true, Chain: nil}}}}); err != nil {
-		return result, validation.NewError("netapp.ResourceClient", "CheckFilePathAvailability", err.Error())
-	}
+    if tracing.IsEnabled() {
+        ctx = tracing.StartSpan(ctx, fqdn + "/ResourceClient.CheckFilePathAvailability")
+        defer func() {
+            sc := -1
+            if result.Response.Response != nil {
+                sc = result.Response.Response.StatusCode
+            }
+            tracing.EndSpan(ctx, sc, err)
+        }()
+    }
+            if err := validation.Validate([]validation.Validation{
+            { TargetValue: body,
+             Constraints: []validation.Constraint{	{Target: "body.Name", Name: validation.Null, Rule: true, Chain: nil },
+            	{Target: "body.ResourceGroup", Name: validation.Null, Rule: true, Chain: nil }}}}); err != nil {
+            return result, validation.NewError("netapp.ResourceClient", "CheckFilePathAvailability", err.Error())
+            }
 
-	req, err := client.CheckFilePathAvailabilityPreparer(ctx, body, location)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "netapp.ResourceClient", "CheckFilePathAvailability", nil, "Failure preparing request")
-		return
-	}
+                req, err := client.CheckFilePathAvailabilityPreparer(ctx, body, location)
+    if err != nil {
+    err = autorest.NewErrorWithError(err, "netapp.ResourceClient", "CheckFilePathAvailability", nil , "Failure preparing request")
+    return
+    }
 
-	resp, err := client.CheckFilePathAvailabilitySender(req)
-	if err != nil {
-		result.Response = autorest.Response{Response: resp}
-		err = autorest.NewErrorWithError(err, "netapp.ResourceClient", "CheckFilePathAvailability", resp, "Failure sending request")
-		return
-	}
+            resp, err := client.CheckFilePathAvailabilitySender(req)
+            if err != nil {
+            result.Response = autorest.Response{Response: resp}
+            err = autorest.NewErrorWithError(err, "netapp.ResourceClient", "CheckFilePathAvailability", resp, "Failure sending request")
+            return
+            }
 
-	result, err = client.CheckFilePathAvailabilityResponder(resp)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "netapp.ResourceClient", "CheckFilePathAvailability", resp, "Failure responding to request")
-	}
+            result, err = client.CheckFilePathAvailabilityResponder(resp)
+            if err != nil {
+            err = autorest.NewErrorWithError(err, "netapp.ResourceClient", "CheckFilePathAvailability", resp, "Failure responding to request")
+            }
 
-	return
-}
+    return
+    }
 
-// CheckFilePathAvailabilityPreparer prepares the CheckFilePathAvailability request.
-func (client ResourceClient) CheckFilePathAvailabilityPreparer(ctx context.Context, body ResourceNameAvailabilityRequest, location string) (*http.Request, error) {
-	pathParameters := map[string]interface{}{
-		"location":       autorest.Encode("path", location),
-		"subscriptionId": autorest.Encode("path", client.SubscriptionID),
-	}
+    // CheckFilePathAvailabilityPreparer prepares the CheckFilePathAvailability request.
+    func (client ResourceClient) CheckFilePathAvailabilityPreparer(ctx context.Context, body ResourceNameAvailabilityRequest, location string) (*http.Request, error) {
+            pathParameters := map[string]interface{} {
+            "location": autorest.Encode("path",location),
+            "subscriptionId": autorest.Encode("path",client.SubscriptionID),
+            }
 
-	const APIVersion = "2019-10-01"
-	queryParameters := map[string]interface{}{
-		"api-version": APIVersion,
-	}
+                        const APIVersion = "2019-10-01"
+        queryParameters := map[string]interface{} {
+        "api-version": APIVersion,
+        }
 
-	preparer := autorest.CreatePreparer(
-		autorest.AsContentType("application/json; charset=utf-8"),
-		autorest.AsPost(),
-		autorest.WithBaseURL(client.BaseURI),
-		autorest.WithPathParameters("/subscriptions/{subscriptionId}/providers/Microsoft.NetApp/locations/{location}/checkFilePathAvailability", pathParameters),
-		autorest.WithJSON(body),
-		autorest.WithQueryParameters(queryParameters))
-	return preparer.Prepare((&http.Request{}).WithContext(ctx))
-}
+        preparer := autorest.CreatePreparer(
+    autorest.AsContentType("application/json; charset=utf-8"),
+    autorest.AsPost(),
+    autorest.WithBaseURL(client.BaseURI),
+    autorest.WithPathParameters("/subscriptions/{subscriptionId}/providers/Microsoft.NetApp/locations/{location}/checkFilePathAvailability",pathParameters),
+    autorest.WithJSON(body),
+    autorest.WithQueryParameters(queryParameters))
+    return preparer.Prepare((&http.Request{}).WithContext(ctx))
+    }
 
-// CheckFilePathAvailabilitySender sends the CheckFilePathAvailability request. The method will close the
-// http.Response Body if it receives an error.
-func (client ResourceClient) CheckFilePathAvailabilitySender(req *http.Request) (*http.Response, error) {
-	sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
-	return autorest.SendWithSender(client, req, sd...)
-}
+    // CheckFilePathAvailabilitySender sends the CheckFilePathAvailability request. The method will close the
+    // http.Response Body if it receives an error.
+    func (client ResourceClient) CheckFilePathAvailabilitySender(req *http.Request) (*http.Response, error) {
+            return client.Send(req, azure.DoRetryWithRegistration(client.Client))
+            }
 
 // CheckFilePathAvailabilityResponder handles the response to the CheckFilePathAvailability request. The method always
 // closes the http.Response Body.
 func (client ResourceClient) CheckFilePathAvailabilityResponder(resp *http.Response) (result ResourceNameAvailability, err error) {
-	err = autorest.Respond(
-		resp,
-		client.ByInspecting(),
-		azure.WithErrorUnlessStatusCode(http.StatusOK),
-		autorest.ByUnmarshallingJSON(&result),
-		autorest.ByClosing())
-	result.Response = autorest.Response{Response: resp}
-	return
-}
+    err = autorest.Respond(
+    resp,
+    client.ByInspecting(),
+    azure.WithErrorUnlessStatusCode(http.StatusOK),
+    autorest.ByUnmarshallingJSON(&result),
+    autorest.ByClosing())
+    result.Response = autorest.Response{Response: resp}
+        return
+    }
 
 // CheckNameAvailability check if a resource name is available.
-// Parameters:
-// body - name availability request.
-// location - the location
+    // Parameters:
+        // body - name availability request.
+        // location - the location
 func (client ResourceClient) CheckNameAvailability(ctx context.Context, body ResourceNameAvailabilityRequest, location string) (result ResourceNameAvailability, err error) {
-	if tracing.IsEnabled() {
-		ctx = tracing.StartSpan(ctx, fqdn+"/ResourceClient.CheckNameAvailability")
-		defer func() {
-			sc := -1
-			if result.Response.Response != nil {
-				sc = result.Response.Response.StatusCode
-			}
-			tracing.EndSpan(ctx, sc, err)
-		}()
-	}
-	if err := validation.Validate([]validation.Validation{
-		{TargetValue: body,
-			Constraints: []validation.Constraint{{Target: "body.Name", Name: validation.Null, Rule: true, Chain: nil},
-				{Target: "body.ResourceGroup", Name: validation.Null, Rule: true, Chain: nil}}}}); err != nil {
-		return result, validation.NewError("netapp.ResourceClient", "CheckNameAvailability", err.Error())
-	}
+    if tracing.IsEnabled() {
+        ctx = tracing.StartSpan(ctx, fqdn + "/ResourceClient.CheckNameAvailability")
+        defer func() {
+            sc := -1
+            if result.Response.Response != nil {
+                sc = result.Response.Response.StatusCode
+            }
+            tracing.EndSpan(ctx, sc, err)
+        }()
+    }
+            if err := validation.Validate([]validation.Validation{
+            { TargetValue: body,
+             Constraints: []validation.Constraint{	{Target: "body.Name", Name: validation.Null, Rule: true, Chain: nil },
+            	{Target: "body.ResourceGroup", Name: validation.Null, Rule: true, Chain: nil }}}}); err != nil {
+            return result, validation.NewError("netapp.ResourceClient", "CheckNameAvailability", err.Error())
+            }
 
-	req, err := client.CheckNameAvailabilityPreparer(ctx, body, location)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "netapp.ResourceClient", "CheckNameAvailability", nil, "Failure preparing request")
-		return
-	}
+                req, err := client.CheckNameAvailabilityPreparer(ctx, body, location)
+    if err != nil {
+    err = autorest.NewErrorWithError(err, "netapp.ResourceClient", "CheckNameAvailability", nil , "Failure preparing request")
+    return
+    }
 
-	resp, err := client.CheckNameAvailabilitySender(req)
-	if err != nil {
-		result.Response = autorest.Response{Response: resp}
-		err = autorest.NewErrorWithError(err, "netapp.ResourceClient", "CheckNameAvailability", resp, "Failure sending request")
-		return
-	}
+            resp, err := client.CheckNameAvailabilitySender(req)
+            if err != nil {
+            result.Response = autorest.Response{Response: resp}
+            err = autorest.NewErrorWithError(err, "netapp.ResourceClient", "CheckNameAvailability", resp, "Failure sending request")
+            return
+            }
 
-	result, err = client.CheckNameAvailabilityResponder(resp)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "netapp.ResourceClient", "CheckNameAvailability", resp, "Failure responding to request")
-	}
+            result, err = client.CheckNameAvailabilityResponder(resp)
+            if err != nil {
+            err = autorest.NewErrorWithError(err, "netapp.ResourceClient", "CheckNameAvailability", resp, "Failure responding to request")
+            }
 
-	return
-}
+    return
+    }
 
-// CheckNameAvailabilityPreparer prepares the CheckNameAvailability request.
-func (client ResourceClient) CheckNameAvailabilityPreparer(ctx context.Context, body ResourceNameAvailabilityRequest, location string) (*http.Request, error) {
-	pathParameters := map[string]interface{}{
-		"location":       autorest.Encode("path", location),
-		"subscriptionId": autorest.Encode("path", client.SubscriptionID),
-	}
+    // CheckNameAvailabilityPreparer prepares the CheckNameAvailability request.
+    func (client ResourceClient) CheckNameAvailabilityPreparer(ctx context.Context, body ResourceNameAvailabilityRequest, location string) (*http.Request, error) {
+            pathParameters := map[string]interface{} {
+            "location": autorest.Encode("path",location),
+            "subscriptionId": autorest.Encode("path",client.SubscriptionID),
+            }
 
-	const APIVersion = "2019-10-01"
-	queryParameters := map[string]interface{}{
-		"api-version": APIVersion,
-	}
+                        const APIVersion = "2019-10-01"
+        queryParameters := map[string]interface{} {
+        "api-version": APIVersion,
+        }
 
-	preparer := autorest.CreatePreparer(
-		autorest.AsContentType("application/json; charset=utf-8"),
-		autorest.AsPost(),
-		autorest.WithBaseURL(client.BaseURI),
-		autorest.WithPathParameters("/subscriptions/{subscriptionId}/providers/Microsoft.NetApp/locations/{location}/checkNameAvailability", pathParameters),
-		autorest.WithJSON(body),
-		autorest.WithQueryParameters(queryParameters))
-	return preparer.Prepare((&http.Request{}).WithContext(ctx))
-}
+        preparer := autorest.CreatePreparer(
+    autorest.AsContentType("application/json; charset=utf-8"),
+    autorest.AsPost(),
+    autorest.WithBaseURL(client.BaseURI),
+    autorest.WithPathParameters("/subscriptions/{subscriptionId}/providers/Microsoft.NetApp/locations/{location}/checkNameAvailability",pathParameters),
+    autorest.WithJSON(body),
+    autorest.WithQueryParameters(queryParameters))
+    return preparer.Prepare((&http.Request{}).WithContext(ctx))
+    }
 
-// CheckNameAvailabilitySender sends the CheckNameAvailability request. The method will close the
-// http.Response Body if it receives an error.
-func (client ResourceClient) CheckNameAvailabilitySender(req *http.Request) (*http.Response, error) {
-	sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
-	return autorest.SendWithSender(client, req, sd...)
-}
+    // CheckNameAvailabilitySender sends the CheckNameAvailability request. The method will close the
+    // http.Response Body if it receives an error.
+    func (client ResourceClient) CheckNameAvailabilitySender(req *http.Request) (*http.Response, error) {
+            return client.Send(req, azure.DoRetryWithRegistration(client.Client))
+            }
 
 // CheckNameAvailabilityResponder handles the response to the CheckNameAvailability request. The method always
 // closes the http.Response Body.
 func (client ResourceClient) CheckNameAvailabilityResponder(resp *http.Response) (result ResourceNameAvailability, err error) {
-	err = autorest.Respond(
-		resp,
-		client.ByInspecting(),
-		azure.WithErrorUnlessStatusCode(http.StatusOK),
-		autorest.ByUnmarshallingJSON(&result),
-		autorest.ByClosing())
-	result.Response = autorest.Response{Response: resp}
-	return
-}
+    err = autorest.Respond(
+    resp,
+    client.ByInspecting(),
+    azure.WithErrorUnlessStatusCode(http.StatusOK),
+    autorest.ByUnmarshallingJSON(&result),
+    autorest.ByClosing())
+    result.Response = autorest.Response{Response: resp}
+        return
+    }
+
