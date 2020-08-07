32211aa4ec9442347767354827fcf8e238385676
SDK Automation <sdkautomation@microsoft.com>
2020-08-07 05:27:49 +0000

    Update from master

--- services/preview/authorization/mgmt/2018-07-01-preview/authorization/roleassignments.go
+++ services/preview/authorization/mgmt/2018-07-01-preview/authorization/roleassignments.go
@@ -542,9 +542,6 @@ func (client RoleAssignmentsClient) List(ctx context.Context, filter string) (re
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "authorization.RoleAssignmentsClient", "List", resp, "Failure responding to request")
 	}
-	if result.ralr.hasNextLink() && result.ralr.IsEmpty() {
-		err = result.NextWithContext(ctx)
-	}
 
 	return
 }
@@ -664,9 +661,6 @@ func (client RoleAssignmentsClient) ListForResource(ctx context.Context, resourc
 	result.ralr, err = client.ListForResourceResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "authorization.RoleAssignmentsClient", "ListForResource", resp, "Failure responding to request")
-	}
-	if result.ralr.hasNextLink() && result.ralr.IsEmpty() {
-		err = result.NextWithContext(ctx)
 	}
 
 	return
@@ -789,9 +783,6 @@ func (client RoleAssignmentsClient) ListForResourceGroup(ctx context.Context, re
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "authorization.RoleAssignmentsClient", "ListForResourceGroup", resp, "Failure responding to request")
 	}
-	if result.ralr.hasNextLink() && result.ralr.IsEmpty() {
-		err = result.NextWithContext(ctx)
-	}
 
 	return
 }
@@ -908,9 +899,6 @@ func (client RoleAssignmentsClient) ListForScope(ctx context.Context, scope stri
 	result.ralr, err = client.ListForScopeResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "authorization.RoleAssignmentsClient", "ListForScope", resp, "Failure responding to request")
-	}
-	if result.ralr.hasNextLink() && result.ralr.IsEmpty() {
-		err = result.NextWithContext(ctx)
 	}
 
 	return
